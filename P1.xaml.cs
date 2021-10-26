5bac7c0f89ea06b10f84f838e01bc31b2bfeb0d3
Александр Выголов <72917288+Shurik711@users.noreply.github.com>
2021-10-26 16:59:16 +0500

    Add files via upload

---
+++ P1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using static System.Windows.Controls.Page;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BFE
+{
+    /// <summary>
+    /// Логика взаимодействия для P1.xaml
+    /// </summary>
+    public partial class P1 : Page
+    {
+        public P1()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            fr1.Navigate(new P2());
+            
+        }
+
+    }
+}
